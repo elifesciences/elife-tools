eff0cb951f7894b67c221e40b53ca2bb78042674
Graham Nott <gnott@starglobal.ca>
2015-06-08 15:10:31 -0700

    Next attempt at coveralls coverage.

--- elifetools/tests/terrain.py
+++ elifetools/tests/terrain.py
@@ -1,4 +1,5 @@
-from lettuce import world
+from lettuce import world, before, after
+import coverage
 import os
 """
 Set world.basedir relative to this terrain.py file,
@@ -7,3 +8,12 @@ and add the directory it to the import path
 """
 world.basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0,world.basedir)
+
+# Start coverage now to capture the most function calls
+world.cover = coverage.coverage()
+world.cover.start()
+
+@after.all
+def save_coverage(total):
+    world.cover.save()
+    world.cover.stop()
