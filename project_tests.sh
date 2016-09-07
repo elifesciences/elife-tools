d45980bf870cdc5242a09df19c7ef6d6150eecc3
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-07 12:31:23 +0100

    Updated project_tests.sh from master to newer develop testing tools

--- project_tests.sh
+++ project_tests.sh
@@ -4,8 +4,6 @@ set -e
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
-pip install lettuce
 pip install coveralls
-cd elifetools/tests
-lettuce
+coverage run -m unittest discover elifetools/tests
 COVERALLS_REPO_TOKEN=$(cat /etc/coveralls/tokens/elife-tools) coveralls
