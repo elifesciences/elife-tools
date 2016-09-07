e49cd180e8ab53271292842ca04c37414dbafa41
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-07 12:26:02 +0100

    Build definition

---
+++ projects_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+pip install coveralls
+cd elifetools/tests
+lettuce
+COVERALLS_REPO_TOKEN=$(cat /etc/coveralls/tokens/elife-tools) coveralls
