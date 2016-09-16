ce35ad618f619753fe305a06c72bad0022a9bf7c
Graham Nott <gnott@starglobal.ca>
2016-09-15 17:27:16 -0700

    Bug fix for edge case (00723) where a fig has no caption.

--- elifetools/parseJATS.py
+++ elifetools/parseJATS.py
@@ -1769,7 +1769,7 @@ def body_block_content(tag):
                     continue
                 if body_block_content_render(block_tag) != {}:
                     tag_content["caption"].append(body_block_content_render(block_tag))
-            if tag_content["caption"] == []:
+            if "caption" in tag_content and tag_content["caption"] == []:
                 del tag_content["caption"]
         # todo!! alt
         set_if_value(tag_content, "alt", "")
