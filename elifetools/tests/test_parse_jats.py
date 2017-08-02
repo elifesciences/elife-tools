a3b62cb026e7c7bcbf84f4a3a112d002cb73e578
Graham Nott <gnott@starglobal.ca>
2017-08-02 13:41:36 -0700

    Two inline table examples, one normal and one key resource table.

--- elifetools/tests/test_parse_jats.py
+++ elifetools/tests/test_parse_jats.py
@@ -1559,6 +1559,49 @@ class TestParseJats(unittest.TestCase):
         OrderedDict([('type', 'figure'), ('assets', [OrderedDict([('type', 'table'), ('doi', u'10.7554/eLife.24231.006'), ('id', u'tbl1'), ('label', u'Table 1'), ('caption', [OrderedDict([('type', 'paragraph'), ('text', u'Summary table of U-Th disequilibrium ages obtained for samples from the Dinaledi Chamber by James Cook University (JCU - <sup>1</sup>) and the University of Melbourne (UoM - <sup>2</sup>). The detailed analytical results are shown in <a href="#tbl2">Tables 2</a> and <a href="#tbl3">3</a>. Sample locations are shown in <a href="#fig1">Figure 1b</a>. The data are ranked by increasing age of the oldest flowstone horizon within the sample, based on the JCU ages. The grey shading highlights the different age groupings observed within the flowstones: 24\u201332 ka, ~50 ka, 88\u2013105 ka, ~242 ka, ~290 ka and &gt;440 ka. Ages are reported relative to 1950.')])]), ('tables', [u'<table><thead><tr><th valign="bottom">Sample ID</th><th valign="bottom">Flowstone group</th><th valign="bottom">Underlying unit</th><th valign="bottom">Age<sup>1</sup> (ka)</th><th valign="bottom">2\u03c3<sup>1</sup> (ka)</th><th valign="bottom">Age<sup>2</sup> (ka)</th><th valign="bottom">2\u03c3<sup>2</sup> (ka)</th></tr></thead><tbody><tr><td class="author-callout-style-b8">\u2003RS19</td><td class="author-callout-style-b8">FS2</td><td class="author-callout-style-b8">sub-unit 3b</td><td class="author-callout-style-b8"><b>24.7</b></td><td class="author-callout-style-b8">0.2</td><td class="author-callout-style-b8"><b>24.53</b></td><td class="author-callout-style-b8">0.43</td></tr></tbody></table>']), ('footnotes', [OrderedDict([('text', [OrderedDict([('type', 'paragraph'), ('text', u'\u2003<sup>1</sup>James Cook University (JCU), Advanced Analytical Centre.')])])]), OrderedDict([('text', [OrderedDict([('type', 'paragraph'), ('text', u'\u2003<sup>2</sup>University of Melbourne (UoM), paleochronology laboratory.')])])])])])])])
         ),
 
+        # example inline table adapted from 00666 kitchen sink
+        ('<root><table-wrap id="inlinetable1" position="anchor"><table frame="hsides" rules="groups"><thead><tr><th/><th>pY</th><th>Experiment</th><th>Concentration (μM)</th></tr></thead><tbody><tr><td>IGF1R-fl + IGF1</td><td>+</td><td><italic>K</italic><sub><italic>m</italic></sub> ATP</td><td>500, 400, 300, 250, 125, 62.5, 31.3, 15.6, 7.8</td></tr></tbody></table><table-wrap-foot><fn><p>This is an unmarked footnote for an anchored/inline table</p></fn></table-wrap-foot></table-wrap></root>',
+        OrderedDict([
+            ('type', 'table'),
+            ('id', u'inlinetable1'),
+            ('tables', [
+                u'<table><thead><tr><th></th><th>pY</th><th>Experiment</th><th>Concentration (\u03bcM)</th></tr></thead><tbody><tr><td>IGF1R-fl + IGF1</td><td>+</td><td><i>K</i><sub><i>m</i></sub> ATP</td><td>500, 400, 300, 250, 125, 62.5, 31.3, 15.6, 7.8</td></tr></tbody></table>'
+            ]),
+            ('footnotes', [
+                OrderedDict([
+                    ('text', [
+                        OrderedDict([
+                            ('type', 'paragraph'),
+                            ('text', u'This is an unmarked footnote for an anchored/inline table')
+                        ])
+                    ])
+                ])
+            ])
+        ])
+        ),
+
+        # example key resources inline table that has a label
+        ('<root><table-wrap id="keyresource" position="anchor"><label>This is a label.</label><table frame="hsides" rules="groups"><thead><tr><th/><th>pY</th><th>Experiment</th><th>Concentration (μM)</th></tr></thead><tbody><tr><td>IGF1R-fl + IGF1</td><td>+</td><td><italic>K</italic><sub><italic>m</italic></sub> ATP</td><td>500, 400, 300, 250, 125, 62.5, 31.3, 15.6, 7.8</td></tr></tbody></table><table-wrap-foot><fn><p>This is an unmarked footnote for an anchored/inline table</p></fn></table-wrap-foot></table-wrap></root>',
+        OrderedDict([
+            ('type', 'table'),
+            ('id', u'keyresource'),
+            ('label', u'This is a label'),
+            ('tables', [
+                u'<table><thead><tr><th></th><th>pY</th><th>Experiment</th><th>Concentration (\u03bcM)</th></tr></thead><tbody><tr><td>IGF1R-fl + IGF1</td><td>+</td><td><i>K</i><sub><i>m</i></sub> ATP</td><td>500, 400, 300, 250, 125, 62.5, 31.3, 15.6, 7.8</td></tr></tbody></table>'
+            ]),
+            ('footnotes', [
+                OrderedDict([
+                    ('text', [
+                        OrderedDict([
+                            ('type', 'paragraph'),
+                            ('text', u'This is an unmarked footnote for an anchored/inline table')
+                        ])
+                    ])
+                ])
+            ])
+        ])
+        ),
+
         )
     def test_body_block_content(self, xml_content, expected):
         soup = parser.parse_xml(xml_content)
