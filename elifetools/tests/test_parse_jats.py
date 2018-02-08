8e853b2d35704e0350cc17bdaa4450883a857089
Graham Nott <gnott@starglobal.ca>
2018-02-07 16:39:40 -0800

    Test case for parsing an inline email address.

--- elifetools/tests/test_parse_jats.py
+++ elifetools/tests/test_parse_jats.py
@@ -1208,6 +1208,33 @@ class TestParseJats(unittest.TestCase):
          [OrderedDict([('type', 'person'), ('name', OrderedDict([('preferred', u'Irawati Kandela'), ('index', u'Kandela, Irawati')])), ('affiliations', [OrderedDict([('name', [u'Developmental Therapeutics Core', u'Northwestern University']), ('address', OrderedDict([('formatted', [u'Evanston', u'United States']), ('components', OrderedDict([('locality', [u'Evanston']), ('country', u'United States')]))]))])]), ('contribution', u'Acquisition of data, Drafting or revising the article'), ('competingInterests', u'Developmental Therapeutics Core is a Science Exchange associated lab')]), OrderedDict([('type', 'person'), ('name', OrderedDict([('preferred', u'Fraser Aird'), ('index', u'Aird, Fraser')])), ('affiliations', [OrderedDict([('name', [u'Developmental Therapeutics Core', u'Northwestern University']), ('address', OrderedDict([('formatted', [u'Evanston', u'United States']), ('components', OrderedDict([('locality', [u'Evanston']), ('country', u'United States')]))]))])]), ('contribution', u'Acquisition of data, Drafting or revising the article'), ('competingInterests', u'Developmental Therapeutics Core is a Science Exchange associated lab')]), OrderedDict([('type', 'group'), ('name', u'Reproducibility Project: Cancer Biology'), ('emailAddresses', [u'tim@cos.io', u'nicole@scienceexchange.com']), ('contribution', u'Analysis and interpretation of data, Drafting or revising the article'), ('competingInterests', u'EI, NP: Employed by and hold shares in Science Exchange Inc.'), ('people', [OrderedDict([('type', 'person'), ('name', OrderedDict([('preferred', u'Elizabeth Iorns'), ('index', u'Iorns, Elizabeth')])), ('affiliations', [OrderedDict([('name', [u'Science Exchange']), ('address', OrderedDict([('formatted', [u'Palo Alto', u'United States']), ('components', OrderedDict([('locality', [u'Palo Alto']), ('country', u'United States')]))]))])])]), OrderedDict([('type', 'person'), ('name', OrderedDict([('preferred', u'Stephen R Williams'), ('index', u'Williams, Stephen R')])), ('affiliations', [OrderedDict([('name', [u'Center for Open Science']), ('address', OrderedDict([('formatted', [u'Charlottesville', u'United States']), ('components', OrderedDict([('locality', [u'Charlottesville']), ('country', u'United States')]))]))])])]), OrderedDict([('type', 'person'), ('name', OrderedDict([('preferred', u'Nicole Perfito'), ('index', u'Perfito, Nicole')])), ('affiliations', [OrderedDict([('name', [u'Science Exchange']), ('address', OrderedDict([('formatted', [u'Palo Alto', u'United States']), ('components', OrderedDict([('locality', [u'Palo Alto']), ('country', u'United States')]))]))])])]), OrderedDict([('type', 'person'), ('name', OrderedDict([('preferred', u'Timothy M Errington'), ('index', u'Errington, Timothy M')])), ('orcid', u'0000-0002-4959-5143'), ('affiliations', [OrderedDict([('name', [u'Center for Open Science']), ('address', OrderedDict([('formatted', [u'Charlottesville', u'United States']), ('components', OrderedDict([('locality', [u'Charlottesville']), ('country', u'United States')]))]))])])])])])]
          ),
 
+        # example of inline email address
+        ('''
+<root xmlns:xlink="http://www.w3.org/1999/xlink">
+<front>
+<article-meta>
+<contrib-group>
+<contrib contrib-type="author" corresp="yes" id="author-1">
+<name>
+<surname>Surname</surname>
+<given-names>FirstName</given-names>
+</name>
+<email>p@example.org</email>
+</contrib>
+</contrib-group>
+</article-meta>
+<front>
+</root>''',
+         [OrderedDict([
+            ('type', 'person'),
+            ('name', OrderedDict([
+                ('preferred', u'FirstName Surname'),
+                ('index', u'Surname, FirstName')])),
+            ('emailAddresses', [u'p@example.org'])
+            ])
+          ]
+         ),
+
         )
     def test_authors_json_edge_cases(self, xml_content, expected):
         soup = parser.parse_xml(xml_content)
