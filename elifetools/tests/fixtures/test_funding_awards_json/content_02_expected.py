23e91cd8df7c31e05c3198f360956a1595dbf072
Graham Nott <gnott@starglobal.ca>
2018-02-26 22:39:09 -0800

    File fixtures for test_funding_awards.

---
+++ elifetools/tests/fixtures/test_funding_awards_json/content_02_expected.py
@@ -0,0 +1,20 @@
+from collections import OrderedDict
+expected = [
+    OrderedDict([
+        ('id', u'fund1'),
+        ('source', OrderedDict([
+            ('funderId', u'10.13039/100000011'),
+            ('name', [u'Howard Hughes Medical Institute'])
+            ])),
+        ('awardId', u'F32 GM089018'),
+        ('recipients', [
+            OrderedDict([
+                ('type', 'person'),
+                ('name', OrderedDict([
+                    ('preferred', u'Melissa Harrison'),
+                    ('index', u'Harrison, Melissa')
+                    ]))
+                ])
+            ])
+        ])
+    ]
