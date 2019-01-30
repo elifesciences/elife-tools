c9ec1c158529e9f8cf6d17ed20008e76355eb046
Graham Nott <gnott@starglobal.ca>
2019-01-30 15:10:27 -0800

    Change to bioRxiv in the pub history test data.

--- elifetools/tests/fixtures/test_pub_history/content_01_expected.py
+++ elifetools/tests/fixtures/test_pub_history/content_01_expected.py
@@ -5,10 +5,10 @@ from elifetools.utils import date_struct
 expected = [
     OrderedDict([
         ('event_type', 'preprint-publication'),
-        ('event_desc', 'This article was originally published as a <ext-link ext-link-type="uri" xlink:href="https://www.biorxiv.org/content/early/2017/03/24/118356">preprint on Biorxiv</ext-link>'),
-        ('event_desc_html', 'This article was originally published as a <a href="https://www.biorxiv.org/content/early/2017/03/24/118356">preprint on Biorxiv</a>'),
+        ('event_desc', 'This article was originally published as a <ext-link ext-link-type="uri" xlink:href="https://www.biorxiv.org/content/early/2017/03/24/118356">preprint on bioRxiv</ext-link>'),
+        ('event_desc_html', 'This article was originally published as a <a href="https://www.biorxiv.org/content/early/2017/03/24/118356">preprint on bioRxiv</a>'),
         ('uri', 'https://www.biorxiv.org/content/early/2017/03/24/118356'),
-        ('uri_text', 'preprint on Biorxiv'),
+        ('uri_text', 'preprint on bioRxiv'),
         ('day', '24'),
         ('month', '03'),
         ('year', '2017'),
