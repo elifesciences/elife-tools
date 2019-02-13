29d1c56b19057cd0629a8fded1dfaad7b62b3876
Graham Nott <gnott@starglobal.ca>
2019-02-13 10:30:11 -0800

    Specific install_requires dependencies.

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ import elifetools
 with open('README.rst') as fp:
     readme = fp.read()
 
-with open('requirements.txt') as f:
-    install_requires = f.read().splitlines()
-
 setup(name='elifetools',
       version=elifetools.__version__,
       description='Tools for using article data in Python.',
       long_description=readme,
       packages=['elifetools'],
       license = 'MIT',
-      install_requires=install_requires,
+      install_requires=[
+        "beautifulsoup4",
+        "lxml",
+        "python-slugify"
+      ],
       url='https://github.com/elifesciences/elife-tools',
       maintainer='eLife Sciences Publications Ltd.',
       maintainer_email='py@elifesciences.org',
