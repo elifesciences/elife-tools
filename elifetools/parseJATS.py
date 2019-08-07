5ba3d7c4ed5c287ccc3994f1b51899a628a9aea8
Graham Nott <gnott@starglobal.ca>
2019-08-06 17:31:36 -0700

    Add article_type and id values to sub_articles.

--- elifetools/parseJATS.py
+++ elifetools/parseJATS.py
@@ -624,6 +624,8 @@ def sub_articles(soup):
     for tag in sub_article_tags:
         sub_article = OrderedDict()
         set_if_value(sub_article, 'doi', sub_article_doi(tag))
+        set_if_value(sub_article, 'article_type', tag.get("article-type"))
+        set_if_value(sub_article, 'id', tag.get("id"))
         set_if_value(sub_article, 'article_title', title(tag))
         if all_contributors(tag):
             sub_article['contributors'] = all_contributors(tag)
@@ -631,6 +633,7 @@ def sub_articles(soup):
         for parent in tag.parents:
             if parent.name == "article":
                 set_if_value(sub_article, 'parent_doi', doi(parent))
+                set_if_value(sub_article, 'parent_article_type', parent.get("article-type"))
                 set_if_value(sub_article, 'parent_article_title', title(parent))
                 set_if_value(sub_article, 'parent_license_url', license_url(parent))
                 break
--- elifetools/tests/fixtures/test_sub_articles/content_01_expected.py
+++ elifetools/tests/fixtures/test_sub_articles/content_01_expected.py
@@ -2,6 +2,8 @@ from collections import OrderedDict
 expected = [
     OrderedDict([
         ('doi', '10.7554/eLife.00666.DL'),
+        ('article_type', 'decision-letter'),
+        ('id', 'SA1'),
         ('article_title', 'Decision letter'),
         ('contributors', [{
             'affiliations': [{
@@ -15,13 +17,17 @@ expected = [
             }]
         ),
         ('parent_doi', '10.7554/eLife.00666'),
+        ('parent_article_type', 'research-article'),
         ('parent_article_title', 'The eLife research article'),
         ('parent_license_url', 'http://creativecommons.org/licenses/by/4.0/')
     ]),
     OrderedDict([
         ('doi', '10.7554/eLife.00666.AR'),
+        ('article_type', 'reply'),
+        ('id', 'SA2'),
         ('article_title', 'Author response'),
         ('parent_doi', '10.7554/eLife.00666'),
+        ('parent_article_type', 'research-article'),
         ('parent_article_title', 'The eLife research article'),
         ('parent_license_url', 'http://creativecommons.org/licenses/by/4.0/')
     ])
