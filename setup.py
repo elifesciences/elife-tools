a522ce7f3b586b16e80e66b2c54b809e442d442e
Luke <lsh-0@users.noreply.github.com>
2020-02-11 14:00:33 +1030

    tox, updates references to python3.5 to python3 in general

--- setup.py
+++ setup.py
@@ -24,5 +24,5 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3",
     ])
