61f6f4f1298b9a653b0252ddd8395b970f6c03aa
Graham Nott <gnott@starglobal.ca>
2025-10-02 20:04:28 -0700

    Add repl() and entity_to_unicode() to utils module

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -424,5 +424,42 @@ class TestUtilsNameFormatting(unittest.TestCase):
         )
 
 
+class TestEntityToUnicode(unittest.TestCase):
+    "tests for entity_to_unicode()"
+
+    def test_entity_to_unicode(self):
+        passes = []
+        passes.append(
+            (
+                "N-terminal &#x03B1;-helix into the heterodimer interface",
+                "N-terminal \u03b1-helix into the heterodimer interface",
+            )
+        )
+
+        passes.append(
+            (
+                "N-terminal &alpha;-helix into the heterodimer interface",
+                "N-terminal \u03b1-helix into the heterodimer interface",
+            )
+        )
+
+        passes.append(
+            (
+                (
+                    "&#x00A0; &#x00C5; &#x00D7; &#x00EF; &#x0394; &#x03B1; &#x03B2; &#x03B3; &#x03BA; "
+                    + "&#x03BB; &#x2212; &#x223C; &alpha; &amp; &beta; &epsilon; &iuml; &ldquo; &ordm; "
+                    + "&rdquo;"
+                ),
+                (
+                    "\xa0 \xc5 \xd7 \xef \u0394 \u03b1 \u03b2 \u03b3 \u03ba \u03bb \u2212 \u223c "
+                    + '\u03b1 &amp; \u03b2 \u03b5 \xcf " \xba "'
+                ),
+            )
+        )
+
+        for string_input, string_output in passes:
+            self.assertEqual(utils.entity_to_unicode(string_input), string_output)
+
+
 if __name__ == "__main__":
     unittest.main()
