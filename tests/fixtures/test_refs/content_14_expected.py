69a4ae05ae18d350d37d7b0a37d7082a7e234cff
Graham Nott <gnott@starglobal.ca>
2026-07-13 18:29:49 -0700

    Parse specific-use attribute of mixed-citation ref tags

---
+++ tests/fixtures/test_refs/content_14_expected.py
@@ -0,0 +1,18 @@
+expected = [
+    {
+        "ref": "Xiaoqiang Huang (2025) Fully computational design of SaCas9 with relaxed PAM requirement. Zenodo. 10.5281/zenodo.18058217",
+        "id": "dataref1",
+        "article_title": "Fully computational design of SaCas9 with relaxed PAM requirement",
+        "full_article_title": "Fully computational design of SaCas9 with relaxed PAM requirement",
+        "reference_id": "10.5281/zenodo.18058217",
+        "doi": "10.5281/zenodo.18058217",
+        "year": "2025",
+        "year-iso-8601-date": "2025",
+        "source": "Zenodo",
+        "publication-type": "data",
+        "authors": [{"group-type": "author", "surname": "Xiaoqiang Huang"}],
+        "specific-use": "generated",
+        "article_doi": None,
+        "position": 1,
+    }
+]
